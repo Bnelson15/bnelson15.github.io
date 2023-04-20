7d0b7aa09e7fa3548d927a13350217fc0ef65b85
Brady nelson <nelson.b2@stu.janesville.k12.wi.us>
2023-04-20 13:43:33 -0500

    update

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -2,11 +2,17 @@ function Reset() {
     document.getElementById("out").value = "";}
 
 function Calculate() {
-var principal= Number(document.getElementById("principle").value);
-var interest= Number(document.getElementById("interest").value);
-var years= Number(document.getElementById("years").value);
-var payment= Number(document.getElementById("Payment").value);
-var answer= principal * interest * years * payment; 
-document.getElementById("out").innerHtml = "$"+ answer ;
+var N= Number((12).value) ;
+var P= Number(document.getElementById("P").value);
+var R= Number(document.getElementById("R").value);
+var T= Number(document.getElementById("T").value);
+var M= Number(document.getElementById("M").value);
+var B= (P(1+R/N)**N*T) - (M [((1+R/N)**N*T) -1])/(R/N);
+document.getElementById("out").innerHTML = "$"+ B ;
 }
+
+document.getElementById("P").setAttribute('size', '6');
+document.getElementById("R").setAttribute('size', '6');
+document.getElementById("T").setAttribute('size', '6');
+document.getElementById("M").setAttribute('size', '6');
 
