6c677303acc4dcd9067270b76cf15881c9653d03
Brady nelson <nelson.b2@stu.janesville.k12.wi.us>
2023-04-24 13:44:07 -0500

    update

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,14 +1,19 @@
 function Reset() {
-    document.getElementById("out").value = "";}
+    document.getElementById("out").innerText = "";
+    document.getElementById("P").value = "";
+    document.getElementById("R").value = "";
+    document.getElementById("T").value = "";
+    document.getElementById("M").value = "";
+}
 
 function Calculate() {
-var N= Number((12).value) ;
+var N= 12;
 var P= Number(document.getElementById("P").value);
 var R= Number(document.getElementById("R").value);
 var T= Number(document.getElementById("T").value);
 var M= Number(document.getElementById("M").value);
-var B= (P(1+R/N)**N*T) - (M [((1+R/N)**N*T) -1])/(R/N);
-document.getElementById("out").innerHTML = "$"+ B ;
+var B= P*(1+(R/N))**(N*T) - (M* [(1+(R/N))**(N*T) -1])/(R/N);
+document.getElementById("out").innerHTML = "$"+ B.toFixed(2) ;
 }
 
 
