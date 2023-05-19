e9a78634e1e068807960b746c14ba582492fb94b
Brady nelson <nelson.b2@stu.janesville.k12.wi.us>
2023-05-19 13:41:32 -0500

    update

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,15 +1,10 @@
-src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"; defer>
 
-var function1 = function(){ 
-    $(document).ready(function(){
-        $("header").animate({
-    
-          left:'250px',
-          opacity:'0.5',
-          height:'150px',
-          width:'150px'
-        });
+$(document).ready(function() {
+    $(".header").animate({
+        left:'250px',
+        opacity:'0.5',
+        height:'150px',
+        width:'150px'
     });
-    
-    };
-    
+});
