6a1b0a2dd52147d46e33027d3073ab8ace2ec65b
Brady nelson <nelson.b2@stu.janesville.k12.wi.us>
2023-05-25 13:39:56 -0500

    update

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -8,3 +8,25 @@ $(document).ready(function() {
         width:'150px'
     });
 });
+
+function ajaxpost () {
+   
+    var data = new FormData(document.getElementById("myForm"));
+   
+    
+    fetch("1c-server.html", { method:"POST", body:data })
+    .then(res => res.text())
+   
+    
+    .then(response => {
+      console.log(response);
+      if (response == "OK") { alert("SUCCESSFUL!"); }
+      else { alert("FAILURE!"); }
+    })
+   
+   
+    .catch(err => console.error(err));
+   
+   
+    return false;
+  }
