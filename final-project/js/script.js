11647efce74e0e13ff763f2927c5377ac908d851
Brady nelson <nelson.b2@stu.janesville.k12.wi.us>
2023-05-26 13:38:57 -0500

    update

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,7 +1,7 @@
 src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"; defer>
 
-$(document).ready(function() {
-    $(".header").animate({
+$(document).onload(function() {
+    $(".parallax").animate({
         left:'250px',
         opacity:'0.5',
         height:'150px',
@@ -9,24 +9,3 @@ $(document).ready(function() {
     });
 });
 
-function ajaxpost () {
-   
-    var data = new FormData(document.getElementById("myForm"));
-   
-    
-    fetch("1c-server.html", { method:"POST", body:data })
-    .then(res => res.text())
-   
-    
-    .then(response => {
-      console.log(response);
-      if (response == "OK") { alert("SUCCESSFUL!"); }
-      else { alert("FAILURE!"); }
-    })
-   
-   
-    .catch(err => console.error(err));
-   
-   
-    return false;
-  }
