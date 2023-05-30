d969beb385a97e05f6eae85cfbd836026d237044
Brady nelson <nelson.b2@stu.janesville.k12.wi.us>
2023-05-30 13:43:57 -0500

    update

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,11 +1,9 @@
-src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"; defer>
 
-$(document).onload(function() {
-    $(".parallax").animate({
-        left:'250px',
-        opacity:'0.5',
-        height:'150px',
-        width:'150px'
+$(document).ready(function(){
+    $(".btn1").click(function(){
+      $("table").slideUp();
     });
-});
-
+    $(".btn2").click(function(){
+      $("table").slideDown();
+    });
+  });
