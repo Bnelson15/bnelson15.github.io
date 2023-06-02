cc6678e2ee27dc63acf8cc9f1e40cab19e0f4db7
Brady nelson <nelson.b2@stu.janesville.k12.wi.us>
2023-06-02 13:08:25 -0500

    update

---
+++ final-project/js/proj.js
@@ -0,0 +1,5 @@
+document.getElementById('showbutton').addEventListener('click', function() {
+var contentContainer = document.getElementById('contentContainer');
+if (contentContainer.style.display==='none')
+{contentContainer.style.display = 'block';}
+else {contentContainer.style.display='none';}});
--- final-project/js/script.js
+++ final-project/js/script.js
@@ -7,3 +7,11 @@ $(document).ready(function(){
       $("table").slideDown();
     });
   });
+
+type="text/javascript"
+  var submitted=false;
+ type="text/javascript"
+  $('#gform').on('submit', function(e) {
+    $('#gform *').fadeOut(2000);
+    $('#gform').prepend('Your submission has been processed...');
+    });
