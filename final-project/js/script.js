7a3ec4ba9f57c00f9335fc6e34759553ac38ab17
Bnelson15 <123410142+Bnelson15@users.noreply.github.com>
2023-06-05 12:30:37 -0500

    update

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -7,11 +7,29 @@ $(document).ready(function(){
       $("table").slideDown();
     });
   });
+  function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
 
-type="text/javascript"
-  var submitted=false;
- type="text/javascript"
-  $('#gform').on('submit', function(e) {
-    $('#gform *').fadeOut(2000);
-    $('#gform').prepend('Your submission has been processed...');
-    });
+    var isValid = true;
+
+    if (name === "") {
+      isValid = false;
+      document.getElementById("name").classList.add("error");
+    } else {
+      document.getElementById("name").classList.remove("error");
+    }
+   if (message === "") {
+      isValid = false;
+      document.getElementById("message").classList.add("error");
+    } else {
+      document.getElementById("message").classList.remove("error");
+    }
+
+    if (isValid) {
+      alert("Form is valid! You can submit it.");
+    } else {
+      alert("Form is not valid. Please fill in all the required fields.");
+    }
+  }
